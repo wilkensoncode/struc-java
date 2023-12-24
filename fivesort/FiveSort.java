ff601403636eaa12108ccf048ba723fc482f4759
Wilkenson <tcbwilkenson@gamilcom>
2023-12-23 22:58:47 -0500

    five sort

---
+++ fivesort/FiveSort.java
@@ -0,0 +1,25 @@
+package fivesort;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class FiveSort {
+    public static void main(String[] args) {
+        System.out.println(fiveSort(new ArrayList<>(List.of(5, 5, 6, 5, 5, 5, 5))));
+    }
+
+    public static List<Integer> fiveSort(List<Integer> array) {
+        int i = 0, j = array.size() - 1;
+
+        while (i < j) {
+            while (array.get(j) == 5)
+                j--;
+            if(array.get(i) == 5 && i < j) {
+                Collections.swap(array, i, j);
+            }
+            i++;
+        }
+        return array;
+    }
+}
