b53b63dd9f331e308fee09d00e5026f24f389fd1
Wilkenson <tcbwilkenson@gamilcom>
2023-12-25 09:42:08 -0500

    linkedList warmup

--- linkedList/Warmup.java
+++ linkedList/Warmup.java
@@ -1,16 +1,18 @@
 package linkedList;
 public class Warmup {
     public static void main(String[] args) {
-        Node link = warmup();
-        while (link != null){
-            System.out.print(link.data + " ");
-            link = link.next;
-        }
+        Node<String> link = warmup();
+//        while (link != null){
+//            System.out.print(link.data + " ");
+//            link = link.next;
+//        }
+
+         printList(link); // recurse
     }
 
     static class Node<T> {
         T data;
-        Node next;
+        Node<T> next;
 
         public Node(T data) {
             this.data = data;
@@ -18,14 +20,20 @@ public class Warmup {
         }
     }
 
-    static Node warmup() {
+    static Node<String> warmup() {
         Node<String> a = new Node<>("a");
         Node<String> b = new Node<>("b");
         Node<String> c = new Node<>("c");
 
         a.next = b;
         b.next = c;
-        c.next = new Node<>(1);
         return a;
+    }
+
+    static Node<String> printList(Node<String> head){
+        if (head == null)
+            return null;
+        System.out.println(head.data);
+        return printList(head.next);
     }
 }
